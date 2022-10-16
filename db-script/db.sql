ee2c4d8468340cf431872469913a16a7e990cfe2
samirZapata <36949423+samirZapata@users.noreply.github.com>
2022-10-15 19:26:49 -0500

    ALL SERVICES REDY

--- db-script/db.sql
+++ db-script/db.sql
@@ -5,15 +5,15 @@
 -- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema unilevel
+-- Schema unilever
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS unilevel DEFAULT CHARACTER SET utf8 ;
-USE unilevel;
+CREATE SCHEMA IF NOT EXISTS unilever DEFAULT CHARACTER SET utf8 ;
+USE unilever;
 
 -- -----------------------------------------------------
--- Table unilevel.mantenimientos_programados
+-- Table unilever.mantenimientos_programados
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.mantenimientos_programados (
+CREATE TABLE IF NOT EXISTS unilever.mantenimientos_programados (
   id_mantenimiento INT(10) NOT NULL AUTO_INCREMENT,
   id_maquina INT(10) NOT NULL,
   fecha_mantenimiento DATE NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE IF NOT EXISTS unilevel.mantenimientos_programados (
 
 
 -- -----------------------------------------------------
--- Table unilevel.linea_produccion
+-- Table unilever.linea_produccion
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.linea_produccion (
+CREATE TABLE IF NOT EXISTS unilever.linea_produccion (
   id_linea INT(10) NOT NULL,
   tipo_linea VARCHAR(45) NOT NULL,
   eficacia INT(35) NOT NULL,
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS unilevel.linea_produccion (
   INDEX fk_linea_produccion_mantenimientos_programados1_idx (mantenimientos_programados_id_maquina ASC) ,
   CONSTRAINT fk_linea_produccion_mantenimientos_programados1
     FOREIGN KEY (mantenimientos_programados_id_maquina)
-    REFERENCES unilevel.mantenimientos_programados (id_mantenimiento)
+    REFERENCES unilever.mantenimientos_programados (id_mantenimiento)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table unilevel.producto
+-- Table unilever.producto
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.producto (
+CREATE TABLE IF NOT EXISTS unilever.producto (
   isbn_producto INT(10) NOT NULL,
   nombre_producto VARCHAR(45) NOT NULL,
   tiempo_produccion DATE NOT NULL,
@@ -51,15 +51,15 @@ CREATE TABLE IF NOT EXISTS unilevel.producto (
   INDEX fk_producto_linea_produccion1_idx (linea_produccion ASC) ,
   CONSTRAINT fk_producto_linea_produccion1
     FOREIGN KEY (linea_produccion)
-    REFERENCES unilevel.linea_produccion (id_linea)
+    REFERENCES unilever.linea_produccion (id_linea)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table unilevel.cantidad_productos
+-- Table unilever.cantidad_productos
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.cantidad_productos (
+CREATE TABLE IF NOT EXISTS unilever.cantidad_productos (
   id_produccion INT(10) NOT NULL AUTO_INCREMENT,
   fecha DATE NOT NULL,
   cantidad_producido INT(40) NOT NULL,
@@ -68,15 +68,15 @@ CREATE TABLE IF NOT EXISTS unilevel.cantidad_productos (
   INDEX fk_cantidad_productos_producto_idx (Isbn_producto ASC) ,
   CONSTRAINT fk_cantidad_productos_producto
     FOREIGN KEY (Isbn_producto)
-    REFERENCES unilevel.producto (isbn_producto)
+    REFERENCES unilever.producto (isbn_producto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table unilevel.remision
+-- Table unilever.remision
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.remision (
+CREATE TABLE IF NOT EXISTS unilever.remision (
   id_pedido INT(10) NOT NULL AUTO_INCREMENT,
   solicitante VARCHAR(45) NOT NULL,
   Direccion_solicitante VARCHAR(45) NOT NULL,
@@ -91,15 +91,15 @@ CREATE TABLE IF NOT EXISTS unilevel.remision (
   INDEX fk_remision_producto1_idx (producto_isbn_producto ASC) ,
   CONSTRAINT fk_remision_producto1
     FOREIGN KEY (producto_isbn_producto)
-    REFERENCES unilevel.producto (isbn_producto)
+    REFERENCES unilever.producto (isbn_producto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table unilevel.distribuidor
+-- Table unilever.distribuidor
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.distribuidor (
+CREATE TABLE IF NOT EXISTS unilever.distribuidor (
   id_distribuidor INT(10) NOT NULL AUTO_INCREMENT,
   nombre_distribuidor VARCHAR(45) NOT NULL,
   rut INT(20) NOT NULL,
@@ -107,13 +107,13 @@ CREATE TABLE IF NOT EXISTS unilevel.distribuidor (
   fecha_estimada_entrega DATE NOT NULL,
   distribuidorcol VARCHAR(45) NOT NULL,
   PRIMARY KEY (id_distribuidor));
-  select * from unilevel.distribuidor;
-DROP TABLE unilevel.distribuidor;
+  select * from unilever.distribuidor;
+DROP TABLE unilever.distribuidor;
 
 -- -----------------------------------------------------
--- Table unilevel.materia_prima
+-- Table unilever.materia_prima
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.materia_prima (
+CREATE TABLE IF NOT EXISTS unilever.materia_prima (
   id_materia INT(10) NOT NULL AUTO_INCREMENT,
   nombre_mateia VARCHAR(45) NOT NULL,
   cantidad_en_stock INT(40) NOT NULL,
@@ -123,16 +123,16 @@ CREATE TABLE IF NOT EXISTS unilevel.materia_prima (
   INDEX fk_materia_prima_distribuidor1_idx (distribuidor_id_distribuidor ASC) ,
   CONSTRAINT fk_materia_prima_distribuidor1
     FOREIGN KEY (distribuidor_id_distribuidor)
-    REFERENCES unilevel.distribuidor (id_distribuidor)
+    REFERENCES unilever.distribuidor (id_distribuidor)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-     DROP TABLE unilevel.materia_prima;
+     DROP TABLE unilever.materia_prima;
 
 
 -- -----------------------------------------------------
--- Table unilevel.tipo_materia_prima
+-- Table unilever.tipo_materia_prima
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS unilevel.tipo_materia_prima (
+CREATE TABLE IF NOT EXISTS unilever.tipo_materia_prima (
   id_producto INT(10) NOT NULL,
   id_materia_prima INT(10) NOT NULL,
   PRIMARY KEY (id_producto, id_materia_prima),
@@ -140,15 +140,15 @@ CREATE TABLE IF NOT EXISTS unilevel.tipo_materia_prima (
   INDEX fk_producto_has_materia_prima_producto1_idx (id_producto ASC),
   CONSTRAINT fk_producto_has_materia_prima_producto1
     FOREIGN KEY (id_producto)
-    REFERENCES unilevel.producto (isbn_producto)
+    REFERENCES unilever.producto (isbn_producto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_producto_has_materia_prima_materia_prima1
     FOREIGN KEY (id_materia_prima)
-    REFERENCES unilevel.materia_prima (id_materia)
+    REFERENCES unilever.materia_prima (id_materia)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-    DROP TABLE unilevel.tipo_materia_prima;
+    DROP TABLE unilever.tipo_materia_prima;
 
 
 
